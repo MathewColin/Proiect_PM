fab564aba3debcfa603452203e4aa53890a582e3
Matei Calin Ceausu <mateicalin2016@gmail.com>
2024-05-27 01:01:55 +0300

    Added lidar

--- CurrentCode.ino
+++ CurrentCode.ino
@@ -9,6 +9,17 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include <LDS_YDLIDAR_X2_X2L.h>
+
+const uint8_t LDS_MOTOR_EN_PIN = 19; // ESP32 Dev Kit LiDAR enable pin
+const uint8_t LDS_MOTOR_PWM_PIN = 15; // LiDAR motor speed control using PWM
+#define LDS_MOTOR_PWM_FREQ    10000
+#define LDS_MOTOR_PWM_BITS    11
+#define LDS_MOTOR_PWM_CHANNEL    2 // ESP32 PWM channel for LiDAR motor speed control
+
+HardwareSerial LidarSerial(2); // TX 17, RX 16
+LDS_YDLIDAR_X2_X2L lidar; 
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET    -1
@@ -28,6 +39,7 @@
 #define AUTONOMOUS 1
 
 bool opmode = 0;
+
 
 #define FACE_COUNT 7
 volatile int face = 0;
@@ -71,12 +83,41 @@ void setup() {
   timerAttachInterrupt(My_timer, &onTimer, true);
   timerAlarmWrite(My_timer, 1000000, true);
   timerAlarmEnable(My_timer);
+
+  Serial.print("LiDAR model ");
+  Serial.println(lidar.getModelName());
+
+  Serial.print("LiDAR RX buffer size "); // default 128 hw + 256 sw
+  Serial.print(LidarSerial.setRxBufferSize(1024)); // must be before .begin()
+
+  uint32_t baud_rate = lidar.getSerialBaudRate();
+  Serial.print(", baud rate ");
+  Serial.println(baud_rate);
+
+  LidarSerial.begin(baud_rate); // Use default GPIO TX 17, RX 16
+
+  lidar.setScanPointCallback(lidar_scan_point_callback);
+  lidar.setPacketCallback(lidar_packet_callback);
+  lidar.setSerialWriteCallback(lidar_serial_write_callback);
+  lidar.setSerialReadCallback(lidar_serial_read_callback);
+  lidar.setMotorPinCallback(lidar_motor_pin_callback);
+  lidar.init();
+
+  LDS::result_t result = lidar.start();
+  Serial.print("LiDAR start() result: ");
+  Serial.println(lidar.resultCodeToString(result));
+
 }
 
 void loop() {
-  if (opmode < 2)
-    detectKeyPress();
+  detectKeyPress();
   drawFace();
+
+  if (opmode == AUTONOMOUS) {
+    lidar.loop();
+    analogWrite(LDS_MOTOR_PWM_PIN, 255);
+  } else
+    analogWrite(LDS_MOTOR_PWM_PIN, 0);
 }
 
 bool keyPress() {
@@ -326,4 +367,79 @@ void drawTriangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x
 
 void fillTriangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color) {
   display.fillTriangle(x0, y0, x1, y1, x2, y2, color);
+}
+
+
+int lidar_serial_read_callback() {
+  int c = LidarSerial.read();
+  return c;
+}
+
+size_t lidar_serial_write_callback(const uint8_t * buffer, size_t length) {
+  return LidarSerial.write(buffer, length);
+}
+
+void lidar_scan_point_callback(float angle_deg, float distance_mm, float quality,
+  bool scan_completed) {
+  static int i=0;
+
+  if ((i++ % 20 == 0) || scan_completed) {
+    Serial.print(i);
+    Serial.print(' ');
+    Serial.print(distance_mm);
+    Serial.print(' ');
+    Serial.print(angle_deg);
+    if (scan_completed)
+      Serial.println('*');
+    else
+      Serial.println();
+  }
+}
+
+void lidar_info_callback(LDS::info_t code, String info) {
+  Serial.print("LDS info ");
+  Serial.print(lidar.infoCodeToString(code));
+  Serial.print(": ");
+  Serial.println(info);
+}
+
+void lidar_error_callback(LDS::result_t code, String aux_info) {
+  Serial.print("LDS error ");
+  Serial.print(lidar.resultCodeToString(code));
+  Serial.print(": ");
+  Serial.println(aux_info);
+}
+
+void lidar_motor_pin_callback(float value, LDS::lds_pin_t lidar_pin) {
+  int pin = (lidar_pin == LDS::LDS_MOTOR_EN_PIN) ?
+    LDS_MOTOR_EN_PIN : LDS_MOTOR_PWM_PIN;
+
+  if (value <= LDS::DIR_INPUT) {
+    // Configure pin direction
+    if (value == LDS::DIR_OUTPUT_PWM) {
+      ledcSetup(LDS_MOTOR_PWM_CHANNEL, LDS_MOTOR_PWM_FREQ, LDS_MOTOR_PWM_BITS);
+      ledcAttachPin(pin, LDS_MOTOR_PWM_CHANNEL);
+    } else
+      pinMode(pin, (value == LDS::DIR_INPUT) ? INPUT : OUTPUT);
+    return;
+  }
+
+  if (value < LDS::VALUE_PWM) // set constant output
+    digitalWrite(pin, (value == LDS::VALUE_HIGH) ? HIGH : LOW);
+  else { // set PWM duty cycle
+    int pwm_value = ((1<<LDS_MOTOR_PWM_BITS)-1)*value;
+    ledcWrite(LDS_MOTOR_PWM_CHANNEL, pwm_value);
+  }
+}
+
+void lidar_packet_callback(uint8_t * packet, uint16_t length, bool scan_completed) {
+  // Uncomment for debug
+  /*
+  Serial.print("Packet ");
+  Serial.print(length);
+  Serial.print("b");
+  float hz = lidar.getCurrentScanFreqHz();
+  Serial.print(", speed ");
+  Serial.println(hz);
+  */
 }
